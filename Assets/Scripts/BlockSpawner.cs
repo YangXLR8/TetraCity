a143dd2285bdd4906def6ab295217a4eeb831790
kmenorio <62768086+kmenorio@users.noreply.github.com>
2024-05-05 15:22:26 +0800

    added blockspawner

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class BlockSpawner : MonoBehaviour
 {
-    [Header("Blocks")]
     [SerializeField] private GameObject[] blocks;
+
+    public static GameObject currentBlock;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentBlock = Instantiate(blocks[Random.Range(0, blocks.Length)],
+                                   transform.position,
+                                   Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Drop"))
+        {
+
+        }
     }
 }
