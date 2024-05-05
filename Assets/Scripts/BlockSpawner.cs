1bc8143283f6ab8392e05fc205d60b4db8571f06
kmenorio <62768086+kmenorio@users.noreply.github.com>
2024-05-05 15:04:44 +0800

    lmao

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BlockSpawner : MonoBehaviour
 {
+    [Header("Blocks")]
+    [SerializeField] private GameObject[] blocks;
+
     // Start is called before the first frame update
     void Start()
     {
