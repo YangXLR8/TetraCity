3b1d624a04f5f83ae617b5d6b2723c8bb8c5f868
Reah <rsardanas@up.edu.ph>
2024-05-05 16:28:00 +0800

    playerController-1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+         // If inputmanager drop button is pressed
+        if (Input.GetButtonDown("Drop"))
+        {
+            // DropBlock();
+        }
+
+        // If inputmanager horizontal negative buttons are pressed
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            // AccessPreviousBlock();
+        }
+
+        // If inputmanager horizontal positive buttons are pressed
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            // AccessNextBlock();
+        }
+
+        // If inputmanager stash buttons are pressed
+        if (Input.GetButtonDown("Stash"))
+        {
+            // AccessStashedBlock();
+        }
+
+        // If inputmanager rotate buttons are pressed
+        if (Input.GetButtonDown("Rotate"))
+        {
+            // RotateBlockClockwise();
+        }
+
     }
 }
