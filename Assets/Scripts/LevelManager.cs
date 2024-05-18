5fb6d4e06eb958851ab045f9aba23345a4487b5c
Reah <rsardanas@up.edu.ph>
2024-05-18 21:19:40 +0800

    subLevels

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button[] buttons;
+    private void Awake()
+    {
+        int unlockecLevel = PlayerPrefs.GetInt("UnlockLevel", 1);
+        for (int i = 0; i < buttons.Length; i++){
+            buttons[i].interactable = false;
+        }
+        for (int i = 0; i < unlockecLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
+    public void OpenLevel(int levelId){
+        string levelName = "Menu Level " + levelId;
+        SceneManager.LoadScene(levelName);
+    }
+}
